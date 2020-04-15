aefb1198e6b2ccd11cd8cb291ea19610de64703f
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-04-15 14:47:37 +0200

    Add method docs

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -20,6 +20,7 @@ type Controller struct {
 	gameServersLister   listersv1.GameServerLister
 }
 
+// NewAgonesController returns a new Controller with informer and lister set. It requires a not nil Agones clientset.
 func NewAgonesController(logger *logrus.Entry, clientSet versioned.Interface) (*Controller, error) {
 	if clientSet == nil {
 		logger.Fatal("controller can't be created with a nil clientSet")
@@ -41,8 +42,11 @@ func NewAgonesController(logger *logrus.Entry, clientSet versioned.Interface) (*
 	return controller, nil
 }
 
+// Run starts the GameServer controller and attach event handlers which will receive notifications from the informer.
 func (c *Controller) Run(stop <-chan struct{}) {
 	c.gameServersInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		// Alternatively, you could set any method that contains the right signature `func(obj interface{})`
+		// AddFunc: c.EventHandlerAdd,
 		AddFunc: func(obj interface{}) {
 			if err := c.EventHandlerGameServerAdd(obj); err != nil {
 				c.logger.WithError(err).Error("add event error")
@@ -66,6 +70,9 @@ func (c *Controller) Run(stop <-chan struct{}) {
 	c.logger.Info("Stopping GameServer Controller")
 }
 
+// EventHandlerGameServerAdd handles events triggered in two occasions:
+// 1. When the controller is first run and has to sync with the cache
+// 2. When a new resource is added via Kubernetes API requests: kubectl, client-go, http requests, etc.
 func (c *Controller) EventHandlerGameServerAdd(obj interface{}) error {
 	key, addedGameServer, err := IsGameServerKind(obj)
 	if err != nil {
@@ -83,6 +90,9 @@ func (c *Controller) EventHandlerGameServerAdd(obj interface{}) error {
 	return nil
 }
 
+// EventHandlerGameServerUpdate handles events triggered due to a resource being updated.
+// That includes chances caused by either the Kubernetes controller manager or any other external actor modifying the
+// resource. I.e.: Another GameServer controller.
 func (c *Controller) EventHandlerGameServerUpdate(oldObj, newObj interface{}) error {
 	oldKey, oldGameServer, err := IsGameServerKind(oldObj)
 	if err != nil {
@@ -103,6 +113,7 @@ func (c *Controller) EventHandlerGameServerUpdate(oldObj, newObj interface{}) er
 	if reflect.DeepEqual(oldGameServer, newGameServer) == false {
 		c.logger.Debugf("Handled Update GameServer Event: %s (%s) - version %s to %s", oldKey, newGameServer.Status.State, oldGameServer.ResourceVersion, newGameServer.ResourceVersion)
 
+		// Both properties from the old and the new GameServer can be accessed. Not only Status.
 		if newGameServer.Status.State == agonesv1.GameServerStateReady && newGameServer.DeletionTimestamp.IsZero() {
 			c.logger.Infof("GameServer Ready %s - %s:%d", newKey, newGameServer.Status.Address, newGameServer.Status.Ports[0].Port)
 		}
@@ -115,6 +126,7 @@ func (c *Controller) EventHandlerGameServerUpdate(oldObj, newObj interface{}) er
 	return nil
 }
 
+// EventHandlerGameServerDelete handles events triggered due to a resource being deleted.
 func (c *Controller) EventHandlerGameServerDelete(obj interface{}) error {
 	key, deletedGameServer, err := IsGameServerKind(obj)
 	if err != nil {
@@ -132,6 +144,9 @@ func (c *Controller) EventHandlerGameServerDelete(obj interface{}) error {
 	return nil
 }
 
+// IsGameServerKind checks if the passed object is of type GameServer and returns the resource key (namespace/name),
+// the GameServer reference. An error is returned if neither a key can be extracted from the object nor the object can
+// be casted to a GameServer type.
 func IsGameServerKind(obj interface{}) (string, *agonesv1.GameServer, error) {
 	var key string
 	var err error

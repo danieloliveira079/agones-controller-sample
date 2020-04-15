64e5accaf2e9d5da299642bf2c0fa46afe69ce5b
Daniel Oliveira <daniel.oliveira079@gmail.com>
2020-04-15 12:26:39 +0200

    Inititate controller from main

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	agonesv1 "agones.dev/agones/pkg/client/clientset/versioned"
 	"flag"
+	"github.com/danieloliveira079/howto-agones-informers/pkg/controller"
+	"github.com/danieloliveira079/howto-agones-informers/pkg/signals"
 	"github.com/sirupsen/logrus"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -24,16 +26,29 @@ func main() {
 
 	logger.Debug("Starting Agones Controller")
 
+	// The account from the Kubeconfig must have the right RBAC configurations.
+	// TODO provide examples of RBAC or point to Agones Docs
 	clientConf, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
-		logger.Fatal(err)
+		logger.WithError(err).Fatal("error building kubeconfig")
 	}
 
+	// Create new AgonesClientSet using the previously created clientConf
 	agonesClientSet, err := agonesv1.NewForConfig(clientConf)
 	if err != nil {
 		logger.Fatal(err)
 	}
 
+	// Instantiates the new agones controller passing the logger and the clientSet to be used by the informer
+	agonesController, err := controller.NewAgonesController(logger, agonesClientSet)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	stop := signals.SetupSignalHandler()
+	agonesController.Run(stop)
+
+	<-stop
 }
 
 func NewLoggerWithLevel(level logrus.Level) *logrus.Entry {
